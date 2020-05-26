d44bc38d54cb365bc7103bbdacbe4527f291bca4
worldclassdev <philip.obosi@eng.uniben.edu>
2020-05-26 10:39:10 +0100

    feature: hero section complete

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,43 @@
+import cx from "classnames";
+import PropTypes from "prop-types";
+import React from "react";
+
+import styles from "./button.module.scss";
+
+interface Props {
+  variant: string;
+  block?: boolean;
+  className?: string;
+  loading?: boolean;
+  rounded?: boolean;
+  shadow?: boolean;
+  disabled?: boolean;
+}
+
+const Button: React.FC<Props> = ({
+  block,
+  className,
+  loading,
+  rounded,
+  shadow,
+  variant,
+  ...props
+}) => {
+  return (
+    <button
+      className={cx(styles.button, className, {
+        [styles.button_block]: block,
+        [styles.button__disabled]: props.disabled && !loading,
+        [styles.button__loading]: loading,
+        [styles.button_rounded]: rounded,
+        [styles.button_shadow]: shadow,
+        [styles[`button_${variant}`]]: variant,
+      })}
+      {...props}
+    >
+      {loading ? <p>Loading...</p> : [props.children]}
+    </button>
+  );
+};
+
+export default Button;
--- src/pages/landing-page/components/Hero.tsx
+++ src/pages/landing-page/components/Hero.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import styles from "./landing-page.module.scss";
+import { Button } from "../../../components/button";
 import { ManualLogo } from "../../../assets/svgs";
+
+import styles from "./landing-page.module.scss";
+
 interface Props {}
 
 const Hero = (props: Props) => {
   return (
     <header className={styles.hero}>
       <ManualLogo className={styles.hero_logo} />
-      <h1 className={styles.hero_title}>BE GOOD TO YOURSELF</h1>
-      <p className={styles.hero_description}>
-        We’re working around the clock to bring you a holistic approach to your
-        wellness. From top to bottom, inside and out.
-      </p>
+      <div className={styles.hero_caption}>
+        <h1 className={styles.hero_caption_title}>BE GOOD TO YOURSELF</h1>
+        <p className={styles.hero_caption_description}>
+          We’re working around the clock to bring you a holistic approach to
+          your wellness. From top to bottom, inside and out.
+        </p>
+        <Button className={styles.hero_caption_button} variant="themed">
+          TAKE THE QUIZ
+        </Button>
+      </div>
     </header>
   );
 };
