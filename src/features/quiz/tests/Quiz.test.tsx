13014d02c15905593564b47b131dc811bc719d24
worldclassdev <philip.obosi@eng.uniben.edu>
2020-05-28 09:56:18 +0100

    cleanups

--- src/features/quiz/tests/Quiz.test.tsx
+++ src/features/quiz/tests/Quiz.test.tsx
@@ -1,7 +1,6 @@
-import React, { useRef } from "react";
-import { render, cleanup } from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
 import Quiz from "../components/Quiz";
-import { Simulate, createRenderer } from "react-dom/test-utils";
 
 const QuizProps = {
   setShowQuizModal: jest.fn(() => {}),
